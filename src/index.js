fc4ac0e9630a3685ece8c121071ee6f46437879a
Taya Hanchar <tayahanchar@gmail.com>
2023-01-16 13:20:25 +0200

    feat: implement morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arrayLetters = [];
+  let itemPosition = 0;
+  let resultArray = [];
+  let result = '';
+
+  while(itemPosition < expr.length) {
+    arrayLetters.push(expr.substr(itemPosition, 10));
+    itemPosition += 10;
+  }
+
+  arrayLetters.forEach(element => decodeElement(element));
+
+  function decodeElement(element) {
+    let position = 0;
+    let wordArray = [];
+    
+    if(element === '**********') {
+      resultArray.push(' ');
+    } else {
+      while(position < element.length) {
+        wordArray.push(element.substr(position, 2));
+        position += 2;
+      }
+      let lett = '';
+      wordArray.forEach(letter => {
+        if(letter === '10') {
+          lett += '.'
+        }
+        if(letter === '11') {
+          lett += '-'
+        }
+      })
+      resultArray.push(lett);
+    }
+  }
+
+  resultArray.forEach(item => {
+    item === ' ' ? result += ' ' : result += MORSE_TABLE[item];
+  })
+
+  return result;
 }
 
 module.exports = {
